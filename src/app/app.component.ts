79b81661fb779cec032c0c82d59967057fe0e209
piyush-chandra0 <piyushchandra41000@gmail.com>
2025-08-08 14:24:03 +0530

    Component Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from "./components/contact/contact.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { AboutComponent } from "./components/about/about.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactComponent, SkillsComponent, ProjectsComponent, AboutComponent, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
